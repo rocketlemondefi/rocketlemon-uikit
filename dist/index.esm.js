548f5842ad45297fd9893ab84fd0a42fb3ba7f3d
Rex Defi <rexdefi@protonmail.com>
2021-10-23 19:22:15 +0100

    changing pic

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1874,8 +1874,8 @@ var Icon$P = function (props) {
 
 var Logo = function (_a) {
     var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
-    return (React.createElement(Svg, __assign({ viewBox: "0 0 257 33" }, props),
-        React.createElement("image", { width: "257", height: "33", href: isDark ? '/images/egg/LogoTextNewDark.png' : '/images/lemon/MainTitle.png' })));
+    return (React.createElement(Svg, __assign({ viewBox: "0 0 205 26" }, props),
+        React.createElement("image", { width: "205", height: "26", href: isDark ? '/images/egg/MainTitleDark.png' : '/images/lemon/MainTitleLight.png' })));
 };
 
 var Icon$Q = function (props) {
